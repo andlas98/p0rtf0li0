9ace6592c25fa0ef0bf3d1e58285b193efdeaaca
Andrew Ohakam <andrewohakam@outlook.com>
2021-05-23 14:22:12 -0400

    Inital commit. started color scheme implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="bodyDiv">
+        <nav>
+          <ul className="navBar">
+            <li>
+              <Link to="/">Homepage</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+  <div>
+    <h2 className="heading andrew">Andrew Ohakam</h2>
+    <p>name</p>
+  </div>);
+}
+
+function About() {
+  return (
+    <div>
+      <h2 className="heading">About</h2>
+      <div className="about-paragraph">
+        My name is Andrew Ohakam. I was born and raised in the Bronx, New York.
+      </div>
     </div>
   );
 }
 
-export default App;
+function Portfolio() {
+  return (
+  <div>
+    <h2>Portfolio</h2>
+  </div>
+  ); 
+}
