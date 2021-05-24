74f7bd1ff89a7c9e0a39f69d37023f8c1454621e
Andrew Ohakam <andrewohakam@outlook.com>
2021-05-24 01:02:09 -0400

    Added info to portfolio page, added pictures

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import portrait from './andrewPortrait.jpg';
 
 export default function App() {
   return (
@@ -47,6 +48,7 @@ function Home() {
   return (
   <div>
     <h2 className="heading andrew">Andrew Ohakam</h2>
+    <img className="homePagePortrait" src={portrait}alt="andrewPortrait"></img>
     <div>
       Andrew Ohakam is a frontend web developer who strives to become a full-stack developer.
     </div>
@@ -68,7 +70,32 @@ function About() {
 function Portfolio() {
   return (
   <div>
-    <h2>Portfolio</h2>
+    <h2 className="heading">Portfolio</h2>
+    <p><b>Note:</b> </p>
+    <div className="project-div">
+      <h3 className="project-heading">Gaming News Aggregator</h3>
+      <p className="description">
+        This webpage displays the RSS feeds of gaming websites I frequent. This prevents me from having to access every single site to stay updated on gaming news.
+      </p>
+      <a href="https://github.com/andlas98/GNA_Redux" target="_blank">Source Code (Github)</a>
+    </div>
+    <div className="project-div">
+      <h3 className="project-heading">My Portfolio Site</h3>
+      <p className="description">
+        The website you're looking at right now! This was actually the result of one the final projects I had to do for my undergrad.
+      </p>
+      <a href="https://github.com/andlas98/final-project" target="_blank">Source Code (Github)</a>
+    </div>
+    <div className="project-div">
+      <h3 className="project-heading">African Students Association Website</h3>
+      <p className="description">
+        In my first college (Rensselaer Polytechnic Institute), I was once webmaster for the African Students Association. The rest of the executive board tasked me to create an official site representing the club. The result was the first full website I ever created (that's also still available on the Internet!).
+      </p>
+      <a href="https://github.com/andlas98/Portfolio/tree/master/ASAWebsite2.0" target="_blank">Source Code (Github)</a>
+      <p></p>
+      <a href="http://asa.union.rpi.edu/" target="_blank">Website</a>
+    </div>
+    
   </div>
   ); 
 }
